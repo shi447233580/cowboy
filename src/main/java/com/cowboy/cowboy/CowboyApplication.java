b176fbbe8bff2541cba8faa50cc518a3618e22ea
Kleene <447233580@qq.com>
2020-09-23 21:33:47 +0800

    s

---
+++ src/main/java/com/cowboy/cowboy/CowboyApplication.java
@@ -0,0 +1,13 @@
+package com.cowboy.cowboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CowboyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CowboyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cowboy/cowboy/CowboyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cowboy.cowboy;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CowboyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
