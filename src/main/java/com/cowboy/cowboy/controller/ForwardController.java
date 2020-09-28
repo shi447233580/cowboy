8b101b5f18186ec59cee39d49741eea28da41199
Kleene <447233580@qq.com>
2020-09-29 00:49:09 +0800

    sss

--- src/main/java/com/cowboy/cowboy/controller/ForwardController.java
+++ src/main/java/com/cowboy/cowboy/controller/ForwardController.java
@@ -1,7 +1,18 @@
 package com.cowboy.cowboy.controller;
 
+import com.cowboy.cowboy.dao.ClerkInfoMapper;
+import com.cowboy.cowboy.model.ClerkInfo;
+import com.cowboy.cowboy.model.ClerkInfoExample;
+import com.cowboy.cowboy.response.ApiResponse;
+import com.cowboy.cowboy.response.ResponseStatus;
+import com.feilong.core.Validator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * @author ss
@@ -11,6 +22,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ForwardController {
 
+    @Autowired
+    private ClerkInfoMapper clerkInfoMapper;
+
 
     /**
      * 跳转至首页
@@ -18,47 +32,66 @@ public class ForwardController {
      * @return
      */
     @RequestMapping("/index")
-    public String toIndex() {
+    public String toIndex(HttpServletRequest request) {
+        ClerkInfoExample clerkInfoExample = new ClerkInfoExample();
+        clerkInfoExample.createCriteria().andClkIdIsNotNull();
+        List<ClerkInfo> lsit = clerkInfoMapper.selectByExample(clerkInfoExample);
+        ClerkInfoExample clerkInfoExample2 = new ClerkInfoExample();
+        clerkInfoExample2.createCriteria().andClkIdIsNotNull();
+        clerkInfoExample2.setOrderByClause("create_time");
+        List<ClerkInfo> lsit2 = clerkInfoMapper.selectByExample(clerkInfoExample);
+
+        if(lsit2.size()>9){
+        List newList = lsit2.subList(0, 9);
+        request.setAttribute("newclerklsit",newList);
+        }else{
+            request.setAttribute("newclerklsit",lsit2);
+        }
+        if(lsit.size()>8){
+            List newList2 = lsit2.subList(0, 8);
+            request.setAttribute("clerklsit",newList2);
+        }else{
+            request.setAttribute("clerklsit",lsit);
+        }
+
         return "index";
 
     }
+
     /**
-     * 跳转至首页2
+     * 查看成员
      *
      * @return
      */
-    @RequestMapping("/index2")
-    public String toIndex2() {
-        return "index2";
-
+    //获取列表
+    @RequestMapping(value = "/prof{id}")
+    public String getFormList(HttpServletRequest request, @PathVariable("id") long id ) {
+        ClerkInfoExample clerkInfoExample = new ClerkInfoExample();
+        clerkInfoExample.createCriteria().andClkIdEqualTo(id);
+        List<ClerkInfo> lsit = clerkInfoMapper.selectByExample(clerkInfoExample);
+        if (Validator.isNullOrEmpty(lsit)) {
+            return "index";
+        }
+        request.setAttribute("clerk",lsit.get(0));
+        return "clerk";
     }
+
+
     /**
-     * 跳转至首页3
+     * 查看所以成员
      *
      * @return
      */
-    @RequestMapping("/index3")
-    public String toIndex3() {
-        return "index3";
-
+    //获取列表
+    @RequestMapping(value = "/Allprof")
+    public String getAllFormList(HttpServletRequest request) {
+        ClerkInfoExample clerkInfoExample = new ClerkInfoExample();
+        clerkInfoExample.createCriteria().andClkIdIsNotNull();
+        List<ClerkInfo> lsit = clerkInfoMapper.selectByExample(clerkInfoExample);
+        if (Validator.isNullOrEmpty(lsit)) {
+            return "index";
+        }
+        request.setAttribute("allclerk",lsit.get(0));
+        return "clerkgroup";
     }
-    /**
-     * 跳转至404
-     *
-     * @return
-     */
-    @RequestMapping("/404")
-    public String to404() {
-        return "404";
-
-    }
-
-    /**
-     * 上传头像页面
-     */
-    @RequestMapping("/uploadHeadImg")
-    public String uploadHeadImg() {
-        return "uploadImg";
-    }
-
 }
---
+++ src/main/java/com/cowboy/cowboy/dao/NewsInfoMapper.java.1
@@ -0,0 +1,30 @@
+package com.cowboy.cowboy.dao;
+
+import com.cowboy.cowboy.model.NewsInfo;
+import com.cowboy.cowboy.model.NewsInfoExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface NewsInfoMapper {
+    int countByExample(NewsInfoExample example);
+
+    int deleteByExample(NewsInfoExample example);
+
+    int deleteByPrimaryKey(Long newsId);
+
+    int insert(NewsInfo record);
+
+    int insertSelective(NewsInfo record);
+
+    List<NewsInfo> selectByExample(NewsInfoExample example);
+
+    NewsInfo selectByPrimaryKey(Long newsId);
+
+    int updateByExampleSelective(@Param("record") NewsInfo record, @Param("example") NewsInfoExample example);
+
+    int updateByExample(@Param("record") NewsInfo record, @Param("example") NewsInfoExample example);
+
+    int updateByPrimaryKeySelective(NewsInfo record);
+
+    int updateByPrimaryKey(NewsInfo record);
+}
